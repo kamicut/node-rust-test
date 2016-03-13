1791decbae6c78e01aed75a287e7fbc000b52549
Marc Farra <marcfarra@gmail.com>
2016-03-13 17:33:29 -0400

    Add image rendering

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 var fractal = require('./src/ffi');
+var mapnik = require("mapnik");
+var fs = require('fs');
 
-console.log(fractal.julia(10));
+var pixels = fractal.julia(256, 256, 0, 0, 4.0, -0.4, 0.6);
+var im = new mapnik.Image(256, 256);
+pixels.forEach(function (pixel, index) {
+  var x = Math.floor(index / 256);
+  var y = index % 256;
+
+  var red = (pixel >> 16) & 0xFF;
+  var green = (pixel >> 8) & 0xFF;
+  var blue = pixel & 0xFF;
+
+  im.setPixel(x, y, new mapnik.Color(red, blue, green));
+})
+
+fs.writeFileSync('image.png', im.encodeSync('png'));
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,9 @@
 extern crate libc;
+extern crate num;
 
 use std::slice;
 use std::mem;
+use num::complex::Complex;
 
 #[repr(C)]
 pub struct Array {
@@ -20,12 +22,59 @@ impl Array {
     }
 }
 
+fn hue_to_rgb(i: f64) -> f64 {
+    let mut h = i;
+    if h > 1.0 {
+        h = h - 1.0;
+    }
+    let ret = match h {
+        h if h < 0.0 => 0.0,
+        h if h < 0.16 => 6.0 * h,
+        h if h < 0.5 => 1.0,
+        h if h < 0.67 => (0.67 - h) * 6.0,
+        _ => 1.0
+    };
+    ret * 255.0
+}
+
+fn pack_rgb(lum: f64) -> i32 {
+    let red = hue_to_rgb(lum/255.0 - 0.3) as i32;
+    let blue = hue_to_rgb(lum/255.0) as i32;
+    let green = hue_to_rgb(lum/255.0 + 0.3) as i32;
+
+    65536 * red + 256 * green + blue
+}
+
 #[no_mangle]
-pub extern fn julia(n: i32) -> Array {
-    let mut vec = Vec::new();
-    for i in 0..n {
-       vec.push(1);
+pub extern fn julia(imgx: i32, imgy: i32, offsetx: f64, offsety: f64,
+    zoom: f64, cRe: f64, cIm: f64) -> Array {
+
+    let max_iterations = 255u16;
+    let scalex = zoom / imgx as f64;
+    let scaley = zoom / imgy as f64;
+
+   let mut v = vec![0; (imgx * imgy) as usize];
+   let c = Complex::new(cRe, cIm);
+
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cy = (y as f64 + offsety) * scaley - (zoom / 2.0);
+            let cx = (x as f64 + offsetx) * scalex - (zoom / 2.0);
+
+            let mut z = Complex::new(cx, cy);
+            let mut i = 0;
+
+            for t in 0..max_iterations {
+                if z.norm() > 2.0 {
+                    break
+                }
+                z = z * z + c;
+                i = t;
+            }
+            let lum = i as f64;
+            v[(x * imgx + y) as usize] = pack_rgb(lum);
+        }
     }
 
-    Array::from_vec(vec)
+    Array::from_vec(v)
 }
